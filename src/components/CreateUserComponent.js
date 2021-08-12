fa11fadcc0dbb2ee727db857fc4325b866c4650d
Robert Gutierrez <robert@ragutierrez.com>
2021-08-12 13:45:06 -0700

    connect create user and create exercise to backend

--- src/components/CreateUserComponent.js
+++ src/components/CreateUserComponent.js
@@ -26,6 +26,9 @@ export default class CreateUser extends Component {
         };
         console.log(newUser);
 
+        axios.post('http://localhost:5000/users/add', newUser)
+            .then(res => console.log(res.data));
+
         this.setState({
             username: ''
         })
